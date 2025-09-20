96de12b58bc49222feadad7e8c62dd66ac867eca
ivanalbizu <ivan.albizu@gmail.com>
2025-09-20 08:56:30 +0200

    feat(readingTime): show readingTime in single post

--- src/utils/reading-time.ts
+++ src/utils/reading-time.ts
@@ -1,5 +1,5 @@
 export function calculateReadingTime(text: string): string {
-  const wordsPerMinute = 200;
+  const wordsPerMinute = 120;
   const words = text.trim().split(/\s+/).length;
   const minutes = Math.ceil(words / wordsPerMinute);
   
