2f75545a82d9e82fc0e21c462ce5c22069d0bf08
ivanalbizu <ivan.albizu@gmail.com>
2025-09-23 19:46:32 +0200

    feat(lang): set lang='en' for content in English

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ export const config: SiteConfig = {
   description: "Maquetador web que le gusta trastear e investigar diferentes \"cacharros\"",
   author: {
     name: "Iván Albizu",
-    bio: "He estudiado FP Superior Aplicaciones móviles Android, desarrollado diferentes sitios con temas propios en Wordpress. Actualmente desarrollando una App Web con nodejs para integrar la Api de Mailjet en el diseño de Newsletter con MJML, usando Autenticación con JWT, base de datos Mongo para Login, Pug para las vistas de la aplicación, expressjs para manejar las rutas, gulp para tareas de la App y alguna otra cosa más.\nRecienteme estaba en departamento de Commerce maquetando diseños a medida de tiendas online construidas mediante Prestashop y Wordpress",
+    bio: "He estudiado FP Superior Aplicaciones móviles Android, desarrollado diferentes sitios con temas propios en Wordpress. Actualmente desarrollando una App Web con nodejs para integrar la Api de Mailjet en el diseño de Newsletter con MJML, usando Autenticación con JWT, base de datos Mongo para Login, Pug para las vistas de la aplicación, expressjs para manejar las rutas, gulp para tareas de la App y alguna otra cosa más.\nRecienteme estaba en departamento de Commerce maquetando diseños a medida de tiendas online construidas mediante Prestashop y Wordpress.",
     // avatar: "/images/avatar.jpg" // Uncomment and add your avatar image to public/images/
   },
   social: [
