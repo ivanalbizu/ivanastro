7b25b3025f9c64fc702714248efee9c3f10d64f3
ivanalbizu <ivan.albizu@gmail.com>
2025-09-25 08:34:10 +0200

    feat(cv): create content for curriculum

--- src/config.ts
+++ src/config.ts
@@ -39,7 +39,7 @@ export const config: SiteConfig = {
     {
       label: "Linkedin",
       name: "linkedin",
-      href: "https://www.linkedin.com/in/ivanalbizu/",
+      href: "https://www.linkedin.com/in/ivanalbizu",
     },
     {
       label: "Twitter",
