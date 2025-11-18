4e9185591527932e6633783666ab0754c1fd4363
ivanalbizu <ivan.albizu@gmail.com>
2025-11-18 19:15:53 +0100

    feat(cobalt-token): add plugin to generate tokens from YAML

---
+++ tokens.config.mjs
@@ -0,0 +1,20 @@
+import pluginCSS from "@cobalt-ui/plugin-css";
+import pluginJS from "@cobalt-ui/plugin-js";
+
+/** @type {import("@cobalt-ui/core").Config} */
+export default {
+  tokens: ["./tokens/colors.yaml", "./tokens/border.yaml", "./tokens/transition.yaml", "./tokens/typography.yaml"],
+  outDir: "./src/styles/tokens/",
+  plugins: [
+    pluginCSS({
+      modeSelectors: [
+        { mode: "light", selectors: ["@media (prefers-color-scheme: light)", '[data-color-mode="light"]'] },
+        { mode: "lightHighContrast", selectors: ["@media (prefers-color-scheme: light) and (prefers-contrast: more)", '[data-color-mode="lightHighContrast"]'] },
+        { mode: "dark", selectors: ["@media (prefers-color-scheme: dark)", '[data-theme="dark"]'] },
+        { mode: "darkHighContrast", selectors: ["@media (prefers-color-scheme: dark) and (prefers-contrast: more)", '[data-color-mode="darkHighContrast"]'] },
+        { mode: "reducedMotion", selectors: ["@media (prefers-reduced-motion: reduce)", '.hola'] }
+      ],
+    }),
+    pluginJS()
+  ],
+};
